d587ac26dbb119fed063b0629ae2517456fdc3a8
Glenn Willen <gwillen@nerdnet.org>
2013-09-21 23:32:30 -0700

    More automatic preprocessing, add General and Extra decks.

--- generatedeck.sh
+++ generatedeck.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-IN=input/TechnicanExtraPool.txt
-OUT=decks/TechnicanExtraPool.xml
-rm -f $OUT
-echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>" >> $OUT
-echo "<mnemosyne core_version=\"1\">" >> $OUT
-awk -f scripts/parse-categorys.awk $IN >> $OUT
-awk -f scripts/parse-question-pool.awk $IN >> $OUT
-echo "</mnemosyne>" >> $OUT
 
+mkdir tmp
+for DECK in Technician General Extra; do
+  IN=input/$DECK.txt
+  TMP=tmp/$DECK.txt
+  OUT=decks/$DECK.xml
+  rm -f $TMP $OUT
+  scripts/stripchars.sh < $IN > $TMP
+  echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>" >> $OUT
+  echo "<mnemosyne core_version=\"1\">" >> $OUT
+  awk -f scripts/parse-categorys.awk $TMP >> $OUT
+  awk -f scripts/parse-question-pool.awk $TMP >> $OUT
+  echo "</mnemosyne>" >> $OUT
+  rm -f $TMP
+done
+rmdir tmp
---
+++ scripts/stripchars.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Translate Latin-1 smart quotes and en-dash into UTF-8. There is probably a
+# better way to do this.
+LANG=C sed -e s/$'\x92'/$'\xe2\x80\x99'/g -e s/$'\x93'/$'\xe2\x80\x9c'/g -e s/$'\x94'/$'\xe2\x80\x9d'/g -e s/$'\x96'/$'\xe2\x80\x93'/g
